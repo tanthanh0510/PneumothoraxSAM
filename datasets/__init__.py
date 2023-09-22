c5cc96d35d533afb137021ccbdbb3227fd4b078e
tanthanh <trantanthanh0510@gmail.com>
2023-09-22 15:23:13 +0700

    add project

---
+++ datasets/__init__.py
@@ -0,0 +1,38 @@
+from .detection import BaseDetectionDataset
+from .instance_seg import BaseInstanceDataset
+from .semantic_seg import BaseSemanticDataset, VOCSemanticDataset, TorchVOCSegmentation, PneumothoraxDataset
+from .transforms import get_transforms
+
+segment_datasets = {'base_ins': BaseInstanceDataset, 'base_sem': BaseSemanticDataset,
+                    'voc_sem': VOCSemanticDataset, 'torch_voc_sem': TorchVOCSegmentation,
+                    'PneumothoraxDataset': PneumothoraxDataset}
+det_dataset = {'base_det': BaseDetectionDataset, }
+
+
+def get_dataset(cfg):
+    name = cfg.name
+    assert name in segment_datasets or name in det_dataset, \
+        print('{name} is not supported, please implement it first.'.format(name=name))
+    transform = get_transforms(cfg.transforms)
+    if name in det_dataset:
+        return det_dataset[name](**cfg.params, transform=transform)
+    target_transform = get_transforms(cfg.target_transforms)
+    return segment_datasets[name](**cfg.params, transform=transform, target_transform=target_transform)
+
+
+class Iterator:
+    def __init__(self, loader):
+        self.loader = loader
+        self.init()
+
+    def init(self):
+        self.iterator = iter(self.loader)
+
+    def get(self):
+        try:
+            data = next(self.iterator)
+        except StopIteration:
+            self.init()
+            data = next(self.iterator)
+
+        return data
---
+++ train.py
@@ -0,0 +1,77 @@
+import argparse
+from omegaconf import OmegaConf
+from torch.utils.data import DataLoader
+from datasets import get_dataset
+from losses import get_losses
+from extend_sam import get_model, get_optimizer, get_scheduler, get_opt_pamams, get_runner
+
+supported_tasks = ['detection', 'semantic_seg', 'instance_seg']
+parser = argparse.ArgumentParser()
+parser.add_argument('--task_name', default='semantic_seg', type=str)
+
+parser.add_argument(
+    '--ckpt_path', default=None, type=str)
+parser.add_argument('--dataset_dir', default=None, type=str)
+
+parser.add_argument('--tensorboard_folder',
+                    default=None, type=str)
+parser.add_argument('--log_folder', default=None, type=str)
+
+parser.add_argument('--model_folder', default=None, type=str)
+parser.add_argument('--cfg', default=None, type=str)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    task_name = args.task_name
+    if args.cfg is not None:
+        config = OmegaConf.load(args.cfg)
+    else:
+        assert task_name in supported_tasks, "Please input the supported task name."
+        config = OmegaConf.load(
+            "./config/{task_name}.yaml".format(task_name=args.task_name))
+
+    train_cfg = config.train
+    if args.dataset_dir:
+        train_cfg.dataset.params.dataset_dir = args.dataset_dir
+    if args.ckpt_path:
+        train_cfg.model.params.ckpt_path = args.ckpt_path
+    if args.tensorboard_folder:
+        train_cfg.tensorboard_folder = args.tensorboard_folder
+    if args.log_folder:
+        train_cfg.log_folder = args.log_folder
+    if args.model_folder:
+        train_cfg.model_folder = args.model_folder
+
+    print('train_cfg: ', train_cfg)
+
+    val_cfg = config.val
+    if args.dataset_dir:
+        val_cfg.dataset.params.dataset_dir = args.dataset_dir
+    print('val_cfg: ', val_cfg)
+    test_cfg = config.test
+    if args.dataset_dir:
+        test_cfg.dataset.params.dataset_dir = args.dataset_dir
+    print('test_cfg: ', test_cfg)
+
+    train_dataset = get_dataset(train_cfg.dataset)
+    train_loader = DataLoader(train_dataset, batch_size=train_cfg.bs, shuffle=True, num_workers=train_cfg.num_workers,
+                              drop_last=train_cfg.drop_last)
+    val_dataset = get_dataset(val_cfg.dataset)
+    val_loader = DataLoader(val_dataset, batch_size=val_cfg.bs, shuffle=False, num_workers=val_cfg.num_workers,
+                            drop_last=val_cfg.drop_last)
+    losses = get_losses(losses=train_cfg.losses)
+    # according the model name to get the adapted model
+    model = get_model(model_name=train_cfg.model.sam_name,
+                      **train_cfg.model.params)
+    opt_params = get_opt_pamams(model, lr_list=train_cfg.opt_params.lr_list, group_keys=train_cfg.opt_params.group_keys,
+                                wd_list=train_cfg.opt_params.wd_list)
+    optimizer = get_optimizer(opt_name=train_cfg.opt_name, params=opt_params, lr=train_cfg.opt_params.lr_default,
+                              momentum=train_cfg.opt_params.momentum, weight_decay=train_cfg.opt_params.wd_default)
+    scheduler = get_scheduler(
+        optimizer=optimizer, lr_scheduler=train_cfg.scheduler_name)
+    runner = get_runner(train_cfg.runner_name)(
+        model, optimizer, losses, train_loader, val_loader, scheduler)
+    # train_step
+    runner.train(train_cfg)
+    if test_cfg.need_test:
+        runner.test(test_cfg)

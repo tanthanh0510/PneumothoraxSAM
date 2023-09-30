ab62ac131a913585fa9867bfb76047423fb29b46
TanThanh <trantanthanh0510@gmail.com>
2023-09-30 21:37:35 +0700

    add file demo and notebook

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     python_requires=">=3.9",
     install_requires=["monai==1.2.0", "pandas==2.1.1", "matplotlib==3.8.0", "scikit-image==0.21.0", "SimpleITK>=2.2.1",
                       "nibabel==5.1.0", "tqdm==4.66.1", "scipy==1.11.3", "ipympl==0.9.3", "opencv-python==4.8.1.78",
-                      "jupyterlab==4.0.6", "ipywidgets==8.1.1", "flask==2.2.3", "flask_cors==3.0.10"],
+                      "jupyterlab==4.0.6", "ipywidgets==8.1.1", "pydicom==2.4.3"],
     packages=find_packages(exclude="notebooks"),
     extras_require={
         "all": ["pycocotools", "opencv-python", "onnx", "onnxruntime"],
